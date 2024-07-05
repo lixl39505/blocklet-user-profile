777100c5a16f6522f99370d99d0cb40d3221adbd
july <408@qq.com>
2024-07-05 11:37:07 +0800

    引入 antd

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,30 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// Andesign
+import { ConfigProvider, App } from 'antd';
+import dayjs from 'dayjs';
+import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
 
+import light from './themes/light';
 import Home from './pages/home';
 
-function App() {
-  return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </div>
-  );
-}
+// 默认中文
+dayjs.locale('zh-cn');
 
 export default function WrappedApp() {
   // While the blocklet is deploy to a sub path, this will be work properly.
   const basename = window?.blocklet?.prefix || '/';
 
   return (
-    <Router basename={basename}>
-      <App />
-    </Router>
+    <ConfigProvider locale={zhCN} theme={light}>
+      <Router basename={basename}>
+        <App className="app">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </App>
+      </Router>
+    </ConfigProvider>
   );
 }
